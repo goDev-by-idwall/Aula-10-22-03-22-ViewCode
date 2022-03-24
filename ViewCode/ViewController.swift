8ef0d8dd4e993aff0694b33a1bb2cf264f0b84a5
Felipe Brigagão de Almeida <felipe-almeida2012@hotail.com>
2022-03-24 13:42:26 -0300

    Finished the ViewCode class

--- ViewCode/ViewController.swift
+++ ViewCode/ViewController.swift
@@ -9,9 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var safeArea: UILayoutGuide!
+    lazy var iconImageView: UIImageView = {
+        let icon = UIImageView()
+        
+        icon.image = UIImage(systemName: "person.fill")
+        icon.tintColor = .green
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        
+        return icon
+    }()
+    
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.textColor = .white
+        label.numberOfLines = 0
+        label.font = UIFont.systemFont(ofSize: 13, weight: .medium)
+        label.textAlignment = .justified
+        label.text = "Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum is simply dummy text of the printing and typesetting industry."
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "View Controller"
+        view.backgroundColor = .darkGray
+        safeArea = view.layoutMarginsGuide
+        
+        view.addSubview(iconImageView)
+        view.addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            iconImageView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 10),
+            iconImageView.heightAnchor.constraint(equalToConstant: 80),
+            iconImageView.widthAnchor.constraint(equalToConstant: 80),
+            iconImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            titleLabel.topAnchor.constraint(equalTo: iconImageView.bottomAnchor, constant: 15),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
+        
     }
 
 
